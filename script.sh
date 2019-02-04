4cdc6d7cabf48ece1e25cc9a68778389f9690ae5
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-04 17:27:20 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-repository="https://github.com/kapils-repos/dummy_repo_1.github.io.git"
+repository1="https://github.com/kapils-repos/dummy_repo_1.github.io.git"
+repository2="https://github.com/kapils-repos/dummy_repo_2.github.io.git"
 localFolder="C:/Git/cloned_repo"
-ECHO $localFolder
+echo $localfolder
