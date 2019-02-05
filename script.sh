1ed1684ad53d564909da9c3a780bde30f4e108b4
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-05 15:21:07 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ echo $localFolder
 git clone $repository1 $localFolder
 cd $localFolder
 git remote add destination $repository2
-git pull destination master --no-edit
+git pull destination master
 git push origin master
