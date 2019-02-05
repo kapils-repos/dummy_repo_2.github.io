a9865d97803ae54ec629834db2b28b8dcddc6d58
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-05 11:47:07 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -2,4 +2,10 @@
 repository1="https://github.com/kapils-repos/dummy_repo_1.github.io.git"
 repository2="https://github.com/kapils-repos/dummy_repo_2.github.io.git"
 localFolder="C:/Git/cloned_repo"
-echo $localfolder
+echo $localFolder
+
+git clone $repository1 $localFolder
+cd $localFolder
+git init
+git remote set-url origin $repository2
+git push origin master
